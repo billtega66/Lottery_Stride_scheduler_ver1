77b19786e409a02311689d7f9e2821eb9cc6f331
Quoc Do <86338979+billtega66@users.noreply.github.com>
2024-12-05 21:38:30 -0500

    Update Process
    final

--- Process.java
+++ Process.java
@@ -1,102 +1,3 @@
-//
-//package scheduler;
-//
-//public class Process {
-//    private static char ID = 'A';
-//    private char jobID;
-//    private int jobLength;
-//    private int tickets;
-//    private int allotmentCount = 0;
-//    private int arrivalTime;
-//    // For stride scheduling
-//    private double pass = 0;
-//    private double stride;
-//    private static final double STRIDE_MULTIPLIER = 10000.0;
-//
-//    public Process() {
-//        jobID = ID++;
-//        jobLength = 30;
-//        tickets = 50;
-//        arrivalTime = 0;
-//        initializeStride();
-//    }
-//
-//    public Process(int minLength, int maxLength,
-//                  int earliestArrival, int latestArrival,
-//                  int minTickets, int maxTickets) {
-//        jobID = ID++;
-//        jobLength = 10*((int)(Math.random()*((maxLength/10)-(minLength/10)))+(minLength/10));
-//        arrivalTime = 10*(int)(Math.random()*((latestArrival/10)-(earliestArrival/10))+(earliestArrival/10));
-//        tickets = (int)(Math.random()*(maxTickets-minTickets))+minTickets;
-//        initializeStride();
-//    }
-//
-//    private void initializeStride() {
-//        this.stride = STRIDE_MULTIPLIER / tickets;
-//    }
-// 
-//    public char getJobID() {
-//        return jobID;
-//    }
-//
-//    public int getJobLength() {
-//        return jobLength;
-//    }
-//
-//    public void setJobLength(int jLength) {
-//        jobLength = jLength;
-//    }
-//
-//    public int getTickets() {
-//        return tickets;
-//    }
-//
-//    public void setTickets(int num) {
-//        tickets = num;
-//    }
-//
-//    public int getAllotmentCount() {
-//        return allotmentCount;
-//    }
-//
-//    public void resetAllotmentCount() {
-//        allotmentCount = 0;
-//    }
-//
-//    public void incAllotmentCount() {
-//        allotmentCount++;
-//    }
-//
-//    public int getArrivalTime() {
-//        return arrivalTime;
-//    }
-//
-//    public void setArrivalTime(int start) {
-//        arrivalTime = start;
-//    }
-//
-//    public double getPass() {
-//        return pass;
-//    }
-//
-//    public void setPass(double pass) {
-//        this.pass = pass;
-//    }
-//
-//    public void updatePass() {
-//        pass += stride;
-//    }
-//
-//    public double getStride() {
-//        return stride;
-//    }
-//
-//    public void printProcess() {
-//        System.out.println("Process: "+jobID+" Arrival Time: "+arrivalTime+
-//                         " Job Length: "+jobLength+" Tickets: "+tickets);
-//    }
-//}
-
 package scheduler;
 
 public class Process {
@@ -117,9 +18,9 @@ public class Process {
 
     public Process() {
         jobID = ID++;
-        jobLength = 30;
+        jobLength = 0;
         originalLength = jobLength;
-        tickets = 50;
+        tickets = 0;
         arrivalTime = 0;
         initializeStride();
     }
